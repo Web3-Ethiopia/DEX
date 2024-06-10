3644e4031499d4853124d68a48024143f97c6651
kirube1992 <kirubelaynalem@gmail.com>
2024-06-10 17:36:58 -0300

    Add-IquotationFetch

---
+++ IquotationFetch.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragema solidity ^0.8.24;
+
+contract IQutotionFetch  {
+
+//Method to get price quote from liquidity  pool 
+    function getPriceQuote(address liquidityPool) external view returns (uint256);
+// metod to view possible price for a quote
+    function getPriceRange(address liquidityPool) external view returns (uint256 lowerBound, uint256 upperbound);
+// Validator method to fetch and verify chainlink price feeds
+    function getMultiHopQuote(address[] memory transactionOrder, uint256 gasLimit) external view returns (uint256);
+// Metod to get multi-hop quote
+    function getMultiHopQuote(address[] memory transactionOrder, uint256 gasLimit) external view returns (uint256);
+ // metod to get swap route 
+    function getSwaproute(address pair1, address pair2) external view returns (uint256);
+
+}
