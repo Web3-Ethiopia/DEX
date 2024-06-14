175ac4489ba5041d90e47e01a907234d29fe0ba6
mikosis123 <mikosis93@gmail.com>
2024-06-14 19:14:52 +0300

    implimentation for pool manager

--- IAIPoolManager.sol
+++ IAIPoolManager.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
+import "./Pool.sol";
 
 interface IAllPoolManager{
 
@@ -9,4 +9,40 @@ interface IAllPoolManager{
     function removeLiquidityFromPool(address outputToken,address pool,uint256 amount)external;
     function collectReward(address pool)external;
     function swap(address inputToken,address outputToken,uint256 amount) external;
+}
+
+contract AllPoolManager is IAllPoolManager {
+    mapping(address => address) public pools; // pool address to pool contract
+
+    function createPool(address inputToken, address outputToken, uint256 fee) external override returns (address pool) {
+        Pool newPool = new Pool(inputToken, outputToken, fee);
+        pool = address(newPool);
+        pools[pool] = pool;
+    }
+
+    function addLiquidityToPool(address inputToken, address poolAddress, uint256 amount) external override {
+        Pool pool = Pool(poolAddress);
+        require(pool.inputToken() == inputToken, "Invalid input token for pool");
+        pool.addLiquidity(msg.sender, amount);
+    }
+
+    function removeLiquidityFromPool(address outputToken, address poolAddress, uint256 amount) external override {
+        Pool pool = Pool(poolAddress);
+        require(pool.outputToken() == outputToken, "Invalid output token for pool");
+        pool.removeLiquidity(msg.sender, amount);
+    }
+
+    function collectReward(address poolAddress) external override {
+        Pool pool = Pool(poolAddress);
+        pool.collectReward(msg.sender);
+    }
+
+    function swap(address inputToken, address outputToken, uint256 amount) external override {
+        
+        address poolAddress = findPool(inputToken, outputToken);
+        require(poolAddress != address(0), "Pool does not exist");
+        Pool pool = Pool(poolAddress);
+        pool.swap(inputToken, outputToken, amount);
+    }
+
 }
