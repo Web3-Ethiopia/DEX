92bf64ecbc0babdca4711e7fd66bb906b4d45f67
XERZES27 <39665735+XERZES27@users.noreply.github.com>
2024-06-20 19:38:09 +0300

    outdated implementation of liquidity manager

--- src/interfaces/IAllPoolManager.sol
+++ src/interfaces/IAllPoolManager.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-
-
-
-interface IAllPoolManager{
-
-    function createPool(address inputToken, address outputToken,uint256 fee)external returns(address pool);
-    function addLiquidityToPool(address inputToken,address pool,uint256 amount) external;
-    function removeLiquidityFromPool(address outputToken,address pool,uint256 amount)external;
-    function collectReward(address pool)external;
-    function swap(address inputToken,address outputToken,uint256 amount) external;
+interface IAllPoolManager {
+    function createPool(address inputToken, address outputToken) external returns (address pool);
+    function addLiquidityToPool(address inputToken, address pool, uint256 amount) external;
+    function removeLiquidityFromPool(address outputToken, address pool, uint256 amount) external;
+    function collectReward(address pool) external;
+    function swap(address inputToken, address outputToken, uint256 amount) external;
 }
