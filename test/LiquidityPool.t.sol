0ab4b3de5ed834613694255d89fe43fa729ae7b3
mikosis123 <mikosis93@gmail.com>
2024-06-29 22:34:04 +0300

    setup for test

--- test/LiquidityPool.t.sol
+++ test/LiquidityPool.t.sol
@@ -1,10 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {LiquidityPool} from "../src/LiquidityPool.sol";
+import {ERC20Mock} from "./mocks/ERC20Mock.sol";
+
 
 contract LiquidityTest {
+ LiquidityPool liquidityPool;
+    ERC20Mock token0;
+    ERC20Mock token1;
+    address owner;
+    address provider;
+    function setUp() public {
+          owner = address(this);
+        provider = address(0x1234);
 
-    function setUp() public {};
+        token0 = new ERC20Mock("Token0", "TK0", owner, 1000 ether);
+        token1 = new ERC20Mock("Token1", "TK1", owner, 1000 ether);
 
-    function test() public {}
+        liquidityPool = new LiquidityPool("ETH/DAI", address(token0), address(token1), 3000, 1000, 2000);
+    };
+    function test() public {};
 }
