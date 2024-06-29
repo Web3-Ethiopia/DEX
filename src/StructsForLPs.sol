d5d67717fde11e87c6cebe7371a61696af02ad22
Daniel Belay Akele <Daniel.BelayA@ethiotelecom.et>
2024-06-29 11:42:12 +0300

    ILiquidityPool

--- src/StructsForLPs.sol
+++ src/StructsForLPs.sol
@@ -2,22 +2,25 @@
 pragma solidity ^0.8.0;
 
 contract StructsForLPs {
-    struct Range {
-        uint256 lowerBound;
-        uint256 upperBound;
-    }
-
-    struct LiquidityPosition {
-        address provider;
-        uint256 amount0;
-        uint256 amount1;
-        Range range;
-        uint256 liquidity;
-    }
-
-    struct PoolState {
+    struct Pool {
+        address token0;
+        address token1;
+        uint24 fee;
         uint256 reserve0;
         uint256 reserve1;
-        uint256 totalLiquidity;
+        uint256 liquidity;
+        PoolPriceRange priceRange;
+    }
+
+    struct PoolPriceRange {
+        uint256 minLowerBound;
+        uint256 maxUpperBound;
+    }
+
+    struct PoolPortion {
+        address poolAddress;
+        uint256 rangeLow;
+        uint256 rangeHigh;
+        uint256 liquidity;
     }
 }
