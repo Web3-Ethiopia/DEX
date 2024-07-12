2605c214a8d72e0ccdfe041659acaaf134420542
abemelekasfaw24 <163566023+abemelekasfaw24@users.noreply.github.com>
2024-07-12 20:18:06 +0300

    Add files via upload

--- src/IMPLEMENTATION OF ISWAPROUTER.sol
+++ src/IMPLEMENTATION OF ISWAPROUTER.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.3;>
+pragma solidity 0.8.0;
 interface IUniswapV2Router01 {
     function factory() external pure returns (address);
     function WETH() external pure returns (address);
