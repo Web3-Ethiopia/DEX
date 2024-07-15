4ca45ba297e27cdf70518c5a7861a6e52c8ce188
abemelekasfaw24 <163566023+abemelekasfaw24@users.noreply.github.com>
2024-07-15 20:16:27 +0300

    Create ISWAPROUTER12.sol

---
+++ src/ISWAPROUTER12.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./ISWAPROUTER12.sol";
+contract ISwapRouter12Test {
+    ISwapRouter12 public router;(
+    event SwapExactTokensForTokens(
+        address indexed tokenIn (0x03cb35fB7D9d34918EFc2921CF2b9BF5ec7F8F59),
+        address indexed tokenOut(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+        uint256 amountIn = 1.45*e13,
+        uint256 amountOutMin =1.0*e13,
+        address path [0x03cb35fB7D9d34918EFc2921CF2b9BF5ec7F8F59],
+        address indexed to (0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+        );
+    event SwapTokensForExactTokens(
+        address indexed tokenIn (0x03cb35fB7D9d34918EFc2921CF2b9BF5ec7F8F59),
+        address indexed tokenOut(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+        uint256 amountIn = 1.45*e13,
+        uint256 amountOutMin =1.0*e13,
+        address path [0x03cb35fB7D9d34918EFc2921CF2b9BF5ec7F8F59],
+        address indexed to (0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+        );
+        );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
