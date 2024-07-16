91c54010f52e41b22e59da7256f4cd354a410819
mikosis123 <mikosis93@gmail.com>
2024-07-16 10:00:47 +0300

    use connect contract abi

---
+++ UIDex/src/hooks/UseConnectContract.tsx
@@ -0,0 +1,31 @@
+import { useReadContract, useWriteContract } from "wagmi";
+import { LIQUIDITY_POOL_ABI, LIQUIDITY_POOL_ADDRESS } from "../constants";
+
+interface UseConnectContractResult {
+  read: (functionName: string, args?: any[]) => any;
+  write: (functionName: string, args?: any[]) => any;
+}
+
+export function useConnectContract(): UseConnectContractResult {
+  const read = (functionName: string, args: any[] = []) => {
+    const result = useReadContract({
+      abi: LIQUIDITY_POOL_ABI,
+      address: LIQUIDITY_POOL_ADDRESS,
+      functionName,
+      args,
+    });
+    return result;
+  };
+
+  const write = (functionName: string, args: any[] = []) => {
+    const result = useWriteContract({
+      abi: LIQUIDITY_POOL_ABI,
+      address: LIQUIDITY_POOL_ADDRESS,
+      functionName,
+      args,
+    });
+    return result;
+  };
+
+  return { read, write };
+}
