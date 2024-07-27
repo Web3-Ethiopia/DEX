65b2c792bf997ccb32c1661edd83003d435c5b80
Sifan23 <sifanyg123@gmail.com>
2024-07-27 16:21:46 +0300

    updated useAddLiqu

--- UIDex/src/hooks/useAddLiquidity.tsx
+++ UIDex/src/hooks/useAddLiquidity.tsx
@@ -1,16 +1,26 @@
-import { useContractWrite } from 'wagmi';
+import { useWriteContract } from 'wagmi';
 import { ALL_POOL_MANAGER_ABI, ALL_POOL_MANAGER_ADDRESS } from '../constants';
 
 export function useAddLiquidity() {
-  const { write, data, error, isLoading, isSuccess } = useContractWrite({
-    address: ALL_POOL_MANAGER_ADDRESS,
+  const { data, error, isSuccess, write } = useWriteContract({
+    address: ALL_POOL_MANAGER_ADDRESS as `0x${string}`, 
     abi: ALL_POOL_MANAGER_ABI,
     functionName: 'addLiquidity',
   });
 
-  const addLiquidity = (name, amount0, amount1, rangeLow, rangeHigh) => {
-    write({ args: [name, amount0, amount1, rangeLow, rangeHigh] });
+  const addLiquidity = async (
+    name: string, 
+    amount0: bigint, 
+    amount1: bigint, 
+    rangeLow: bigint, 
+    rangeHigh: bigint
+  ) => {
+    try {
+      await write?.({ args: [name, amount0, amount1, rangeLow, rangeHigh] });
+    } catch (err: unknown) {
+      console.error('Error writing to contract:', err);
+    }
   };
 
-  return { addLiquidity, data, error, isLoading, isSuccess };
+  return { addLiquidity, data, error, isSuccess };
 }
